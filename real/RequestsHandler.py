7d0d7a2228fa097e70b9bf2617d0ebe8bc1427c3
fizzzgen <40318860+fizzzgen@users.noreply.github.com>
2018-08-15 23:04:35 +0300

    Socket version
    1 kHz streaming speed

--- real/RequestsHandler.py
+++ real/RequestsHandler.py
@@ -1,26 +1,71 @@
 #!/usr/bin/python3
 
 
-
-
-
 import json
-import cgi
-print("Content-type: text/html\n")
+import socket
+from multiprocessing import Process, Manager
 
-currentPinDictionary = {}
-handledPinDictionary = {}
-form = cgi.FieldStorage()
-if(form["Type"].value=="SendConf"):
-    handledPinDictionary = json.load(form["Dict"])
-else:
-    for i in range(70):
-        if i%4==0:
-            currentPinDictionary[i] = ["DigitalOutput",1]
-        if i%4==1:
-            currentPinDictionary[i] = ["DigitalInput",0]
-        if i%4==2:
-            currentPinDictionary[i] = ["AnalogOutput", 1028]
-        if i%4==3:
-            currentPinDictionary[i] = ["AnalogInput",0]
-    print(json.dump(currentPinDictionary))
+
+manager = Manager()
+currentPinDictionary = manager.dict()
+handledPinDictionary = manager.dict()
+
+def getConfHandler():
+    sock = socket.socket()
+    sock.bind(('', 11117))
+    sock.listen(100)
+    conn, addr = sock.accept()
+    while True:
+        data = conn.recv(2048)
+        if not data:
+            continue
+        try:
+            string = str(data.decode())
+            #print("-----getConfHandler SUCCESS: decode success-----")
+            while string[-1]==" ":
+                    string = string[:-1]
+        except:
+            print("-----getConfHandler ERROR: Decode error-----")
+            continue
+        try:
+            if string=="getConf":
+                send_data = json.dumps(dict(currentPinDictionary))
+                while len(send_data)<2048:
+                    send_data+=" "
+                conn.send(send_data.encode())
+            #print("-----getConfHandler SUCCESS: Dictionary sent!-----")
+        except:
+            print("-----getConfHandler ERROR: Dictionary didn't send-----")
+
+def sendConfHandler():
+    sock = socket.socket()
+    sock.bind(('', 11118))
+    sock.listen(100)
+    conn, addr = sock.accept()
+    while True:
+        data = conn.recv(2048)
+        if not data:
+            continue
+        try:
+            string = str(data.decode())
+            #print("-----sendConfHandler SUCCESS: Decode success-----")
+            while string[-1]==" ":
+                    string = string[:-1]
+        except:
+            print("-----sendConfHandler ERROR: Decode error-----")
+            continue
+        try:
+            if string[0]=="{":
+                while string[-1]!="}":
+                    string = string[:-1]
+                handledPinDictionary.update(json.loads(string))
+            #print("-----sendConfHandler SUCCESS: Dictionary handled!-----")
+            currentPinDictionary = handledPinDictionary
+        except:
+            print("-----sendConfHandler ERROR:Dictionary couldn't be handeled!-----")
+    
+procs = [ Process(target=getConfHandler),Process(target=sendConfHandler) ]
+for proc in procs:
+    proc.start()
+for proc in procs:
+    proc.join()

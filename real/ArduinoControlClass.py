9eb6f4c8590c6b7632a11dab4cddd45097783031
fizzzgen <40318860+fizzzgen@users.noreply.github.com>
2018-08-15 23:02:03 +0300

    Socket versions
    1 kHz streaming speed

--- real/ArduinoControlClass.py
+++ real/ArduinoControlClass.py
@@ -1,6 +1,7 @@
 import requests
 import json
-
+import time
+import socket
 def arduinoMakePin(PinMode, PinValue=0):  # just making tuple
     return list((PinMode, PinValue))
 
@@ -10,6 +11,10 @@ class ArduinoControl:
     def __init__(self, RobotAdress):
         self.PinDictionary = {}
         self.RobotAdress = RobotAdress
+        self.sockGet = socket.socket()
+        self.sockGet.connect((RobotAdress, 11117))
+        self.sockSend = socket.socket()
+        self.sockSend.connect((RobotAdress, 11118))
 
     def makeSamplePinDictionary(self):  # just for debugging
         for i in range(70):
@@ -56,15 +61,65 @@ class ArduinoControl:
             print("Something went wrong with PinRead!")
 
     def getConfiguration(self):  # will get config of pins from Raspberry and make PinDictionary like it is
-        response = requests.post(self.RobotAdress, data={"Type":"GetConf"})
-        self.PinDictionary = json.load(response.text)
-        print(self.PinDictionary)
+        #time_to_get = time.time()
+        msg = "getConf"
+        totalsent = 0
+        while(len(msg)<2048):
+            msg+=" "
+        msg = msg.encode()
+        while totalsent < 2048:
+            sent = self.sockGet.send(msg[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
+        data = self.myreceive()
+        if not data:
+            print("Connection lost")
+            exit(0)
+        string = data
+        while string[-1]!="}":
+            string = string[:-1]
+        #print("get:"+str(time.time()-time_to_get))
+        data = str(string)
+        self.PinDictionary = json.loads(data)
+        #print(self.PinDictionary)
         return
 
     def sendConfiguration(self):  # will send config of pins from this scratch and make PinDictionary on Raspberry like it is
-        data = {}
-        data["Type"] = "SendConf"
-        data["Dict"]=json.dump(self.PinDictionary)
-        response = requests.post(self.RobotAdress, data=data)
-        print(response.text)
+        #time_to_send = time.time()
+        totalsent = 0
+        msg = json.dumps(self.PinDictionary)
+        while(len(msg)<2048):
+            msg+=" "
+        msg = msg.encode()
+        while totalsent < 2048:
+            sent = self.sockSend.send(msg[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
+        
+        #print("send:"+str(time.time()-time_to_send))
+        
         return
+    def myreceive(self):
+        chunks = ""
+        bytes_recd = 0
+        while bytes_recd < 2048:
+            chunk = self.sockGet.recv(min(2048 - bytes_recd, 2048))
+            if chunk == '':
+                raise RuntimeError("socket connection broken")
+            chunk = chunk.decode()
+            chunks+=(chunk)
+            bytes_recd = bytes_recd + len(chunk)
+        return chunks
+AC = ArduinoControl("localhost")
+AC.makeSamplePinDictionary()
+time0 = time.time()
+AC.sendConfiguration()
+for i in range(100):
+    AC.getConfiguration()
+    AC.sendConfiguration()
+time1 = time.time()
+print((time1-time0)/200.0)
+
+
